e2ef189ca38163d570f4d6131bd1a4ab49c826bd
Lucia Martinez <Luciamartinez.cv@gmail.com>
2024-02-08 20:28:15 -0300

    Añadir nuevos archivos desde la PC

--- project/config/settings.py
+++ project/config/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core",
-    "cliente",
+    'core.apps.CoreConfig',
+    'cliente.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
